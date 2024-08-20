ef642123e91f41abf15e76738daf6c7fbc6d4dc4
Kaique de Souza Pereira <130515546+KaiqueSPereira@users.noreply.github.com>
2024-08-20 20:23:58 -0300

    Add files via upload

---
+++ AuladeC.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (int argc, char *argv[]){
+	printf("Hello World \n");
+	int retorno = printf("Olá Mundo!!! \n");
+	printf("%d",retorno); // tamanho da string
+	retorno = printf("Aulas iniciais de C \n");
+	printf("%d",retorno); // tamanho da string
+	
+	
+	return 0;
+	
+}
+
+//2 - estrutura da função printf()
+		
